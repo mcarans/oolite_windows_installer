12ea5e79c72d742a623991d73313e8c9537ac0dd
Mike <rans@email.com>
2025-10-22 18:33:24 +1300

    Don't build NSPR

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ pacboy -S binutils --noconfirm
 
 cd packages
 
-package_names=(gnustep-make gnustep-base nspr spidermonkey pcaudiolib espeak-ng SDL)
+package_names=(gnustep-make gnustep-base spidermonkey pcaudiolib espeak-ng SDL)
 
 for packagename in "${package_names[@]}"; do
     echo "Installing $packagename package"
